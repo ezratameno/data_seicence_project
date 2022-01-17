c7069587de813dc5278996eefdb3d03ed73d65f3
ezratameno <ezratameno@gmail.com>
2022-01-17 20:04:54 +0200

    improve crawling

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from os import error
+from time import sleep
 from bs4 import BeautifulSoup
 from numpy import string_
+import numpy
 import requests
 import datetime
 import pandas as pd
 
-cols={"season","compettion", "event", "compettor", "pos", "medal","sex", "noc", "year of birth","age"}
+cols={"season","compettion_year", "city", "event", "compettor", "pos", "medal","sex", "noc", "year of birth","age", "cm", "kg"}
 df=pd.DataFrame(columns=cols)
     
     
 def main():
-    # createCsv()
-    # df.to_csv("res.csv")
-    at = pd.read_csv("res.csv")
+    createCsv()
+    df.to_csv("res.csv")
+    # at = pd.read_csv("res.csv")
 
    
 domain = "http://www.olympedia.org"
@@ -25,29 +27,28 @@ def createCsv():
     container = soup.find("div", attrs={"class": "container"})
     tables = container.findAll("table", attrs={"class": "table table-striped"})
     summerOlympic = tables[0]
-    winterOlympic = tables[1]
     try:    
         findCompettionNameAndLink(summerOlympic, "summer")
     except Exception as e:
         print(e)
-    # findCompettionNameAndLink(winterOlympic, "winter")
 
 # first page
 def findCompettionNameAndLink(olympicType, season):
     for tr in olympicType.findAll("tr"):
         td = tr.find("td")
-        compettion = td.text
-        compettionYear = compettion.split()[len(compettion.split())-1]
+        str1 = " "
+        city = str1.join(td.text.split()[0:len(td.text.split()) -1])
+        compettionYear = td.text.split()[len(td.text.split())-1]
         link = domain + td.find("a")['href']
         try:    
-            getEvent(link, season, compettion, compettionYear)
+            getEvent(link, season, city, compettionYear)
         except Exception as e:
             print(e)
         
 
 # second page
 
-def getEvent(link, season, compettion, compettionYear):
+def getEvent(link, season, city, compettionYear):
     response = requests.get(link)
     soup = BeautifulSoup(response.content, "html.parser")
     container = soup.find("div", attrs={"class": "container"})
@@ -58,7 +59,7 @@ def getEvent(link, season, compettion, compettionYear):
         eventName = a.text
         link = domain + a['href']
         try:    
-            getEventDeatails(link, season, compettion, eventName, compettionYear)
+            getEventDeatails(link, season, city, eventName, compettionYear)
         except Exception as e:
             print(e)
             
@@ -67,22 +68,23 @@ def getEvent(link, season, compettion, compettionYear):
         a = tr.find("td").a
         eventName = a.text
         newLink = domain + a['href']
-        getEventDeatails(link, season, compettion, eventName,compettionYear)
+        getEventDeatails(link, season, city, eventName,compettionYear)
       
 # third page
-def getEventDeatails(link, season, compettion, eventName,compettionYear):
+def getEventDeatails(link, season, city, eventName,compettionYear):
     response = requests.get(link)
     soup = BeautifulSoup(response.content, "html.parser")
     container = soup.find("div", attrs={"class": "container"})
     table = container.find("table", attrs={"class": "table table-striped"})
     trs = table.findAll("tr")
     numOfCols = len(trs[0].findAll("th"))
-    print("numOfCols: ", numOfCols, " ,compettion: ", compettion, " ,eventName: ",eventName)
+    print("numOfCols: ", numOfCols, " ,city: ", 
+          city, " ,compettionYear: ", compettionYear,
+          " ,eventName: " , eventName)
     
     if len(trs) > 0:
        posOfCompetitorName = findIndexOfCDetails(trs[0])
        print("posOfCompetitorName: ", posOfCompetitorName)
-
         #get rid of headline
         # trs = trs[1:]
        for tr in trs:
@@ -96,55 +98,69 @@ def getEventDeatails(link, season, compettion, eventName,compettionYear):
                   compettorName = a.text
 
                   if pos == "1" or pos == "=1":
-                      medal = "Gold"
+                    medal = "Gold"
                   elif  pos == "2" or pos == "=2":
                       medal = "Silver"
                   elif  pos == "3" or pos == "=3":
                       medal = "Bronze"
                   else:
-                      medal = "None"
+                      medal = numpy.nan
                   try:    
-                    getCompettorData(compettorLink, season, compettion, eventName, compettorName, pos, medal,compettionYear)
+                    getCompettorData(compettorLink, season, city, eventName, compettorName, pos, medal,compettionYear)
+                    global df
+                    df.to_csv("res.csv")
+
+
                   except Exception as e:
                       print(e)
+
+
 
 def findIndexOfCDetails(tr):
     ths = tr.findAll("th")
     indexOfName = 0
     for i in range(0,len(ths)):
-        if ths[i].text == "Competitor(s)" or ths[i].text == "Player" or ths[i].text == "Athlete":
-            indexOfName = i
-        
-    return indexOfName
+        if ths[i].text == "Competitor(s)" or ths[i].text == "Player" or ths[i].text == "Athlete" or ths[i].text == "Swimmer" or ths[i].text == "Judoka" or ths[i].text == "Gymnast":
+            return i    
+    return numpy.nan
 
-def getCompettorData(link, season, compettion, eventName, compettorName, pos, medal,compettionYear):
+def getCompettorData(link, season, city, eventName, compettorName, pos, medal,compettionYear):
     response = requests.get(link)
     soup = BeautifulSoup(response.content, "html.parser")
     container = soup.find("div", attrs={"class": "container"})
     table = container.find("table",attrs={"class":"biodata"})
     trs = table.findAll("tr")
     # for unknown values
-    sex = "None"
-    noc = "None"
-    yearOfBirth = "None"
-    age = "None"
+    sex = numpy.nan
+    noc = numpy.nan
+    cm = numpy.nan
+    kg = numpy.nan
+
+    yearOfBirth =numpy.nan
+    age = numpy.nan
     for tr in trs:
         if tr.find("th").text == "Sex":
             sex = tr.find("td").text
         if tr.find("th").text == "NOC":
             noc = tr.find("td").text
+        if tr.find("th").text == "Measurements":
+            measurements = tr.find("td").text
+            if "/" in measurements:
+                cm = measurements.split("/")[0].split()[0]
+                kg = measurements.split("/")[1].split()[0].split("-")[0]
         if tr.find("th").text == "Born":
             date = tr.find("td").text.split()[:3]
             if str.isdigit(date[len(date)-1]):
                 yearOfBirth = date[len(date)-1]
                 age = int(compettionYear) - int(yearOfBirth)
-    newEntry = {'season': [season],'compettion': [compettion],
+    newEntry = {'season': [season],'city': [city], 'compettion_year': [compettionYear],
     'event': [eventName], 'compettor': [compettorName],'pos': [pos],
     'medal': [medal], 'sex': [sex],'noc': [noc], 'year of birth': [yearOfBirth],
-    'age': [age]}
+    'age': [age], 'cm': [cm], 'kg': [kg]}
     dataFrame2 = pd.DataFrame(newEntry,columns=cols)
     global df
     df = df.append(dataFrame2)
+    sleep(1)
       
 if __name__ == "__main__":
     
